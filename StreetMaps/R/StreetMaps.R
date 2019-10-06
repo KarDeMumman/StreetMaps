3adb8f384b9d02092524940bf926ad4993830d3d
KarDeMumman <sarah.walid.alsaadi@liu.se>
2019-10-06 15:11:15 +0200

    Wrote the api function

--- StreetMaps/R/StreetMaps.R
+++ StreetMaps/R/StreetMaps.R
@@ -1,24 +1,32 @@
 library(httr)
+library(leaflet)
+library(rjson)
+library(jsonlite)
 
+#' Draw the municipality boundaries
+#' @param dataset one of the following (fi-8, ch-8, no-4, no-7, dk-4, se-4, se-7, us-4, gl-7,world-2)
+#'
+#' @return The object of class Maps_api.
+#' @examples
+#' \dontrun{
+#' Maps_api("se-4")
+#' Maps_api("se-7")}
 
-
-Maps_api <- function(path) {
+Maps_api <- function(dataset) {
+  path<-sprintf("/v2/%s/geo/2000", dataset)
   url <- modify_url("http://api.thenmap.net", path = path)
   ua <- user_agent("http://github.com/KarDeMumman/StreetMaps")
-  
- 
   resp<-GET(url,ua)
-  
   if (http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
   
-  parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
+  parsed <- fromJSON(content(resp, "text"))
   
   if (http_error(resp)) {
     stop(
       sprintf(
-        "GitHub API request failed [%s]\n%s\n<%s>", 
+        "Thenmap API request failed [%s]\n%s\n<%s>", 
         status_code(resp),
         parsed$message,
         parsed$documentation_url
@@ -27,24 +35,19 @@ Maps_api <- function(path) {
     )
   }
   
-    
+  map <- leaflet()
+  map <- addTiles(map)
+  map <- setView(map, lng = 18.961619, lat = 58.298584, zoom = 4)
+  map <- addGeoJSON(map, parsed)
+  
   structure(
     list(
       content = parsed,
       path = path,
-      response = resp
+      response = resp,
+      map = map
     ),
     class = "Maps_api"
   )
 }
-
-print.Maps_api <- function(x, ...) {
-  cat("<GitHub ", x$path, ">\n", sep = "")
-  str(x$content)
-  invisible(x)
-} 
-  
-
-#resp <- Maps_api("/v2/se-7/data/1974")
-
 
